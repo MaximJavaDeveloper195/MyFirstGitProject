e4e5eeefcd3b13a14eef61f76f0d7b670fced08d
MaximJavaDeveloper195 <maximdudarev22@gmail.com>
2022-08-11 14:14:47 +0300

    3

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -11,6 +11,5 @@ public class MainWindow extends JFrame {
     }
     public static void main(String[] args) {
         MainWindow mainWindow = new MainWindow();
-        GameField gameField = new GameField();
     }
 }
